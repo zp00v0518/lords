319e8c74e49e2103bdc1bbc83fc8d48dae42aad7
Dmitriy <you@example.com>
2019-01-03 22:57:25 +0200

    canvas draw map

--- src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
+++ src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
@@ -1,37 +1,57 @@
 <template>
- <div>
-   <canvas ref='scene' :style="{width: widthScene, height: heightScene}"> </canvas>
- </div>
+  <div class="globalmap">
+    <canvas ref="scene" :width="widthScene" :height="heightScene"></canvas>
+    <button id="left" class="globalmap__move globalmap__move-left" type="button" @click="moveOnMap">left</button>
+    <button id="top" class="globalmap__move globalmap__move-top" type="button" @click="moveOnMap">top</button>
+    <button id="right" class="globalmap__move globalmap__move-right" type="button" @click="moveOnMap">right</button>
+    <button id="bottom" class="globalmap__move globalmap__move-bottom" type="button" @click="moveOnMap">bottom</button>
+  </div>
 </template>
 
 <script>
-import {drawMap} from '../modules';
+import { drawMap } from "../modules";
 export default {
   name: "GlobalMap",
-  props: ['widthScene', 'heightScene'],
+  props: ["widthScene", "heightScene"],
   data() {
     return {
       ctx: null,
-      currentMap:[],
+      currentMap: []
     };
   },
-  created() {
-  },
+  created() {},
   watch: {
     "$store.state.globalMap.currentMap": function() {
       this.currentMap = this.$store.state.globalMap.currentMap;
-    this.drawMap()
-
-    },
+      this.drawMap();
+    }
   },
-  computed: {},
+  computed: {
+    tileWidth() {
+      const intermediate =
+        parseInt(this.widthScene) / 2 / (this.currentMap.length / 2);
+      return intermediate / (this.currentMap.length / 2) + intermediate;
+    },
+    isoCoords() {
+      const x = -(this.tileWidth / 2);
+      const y = parseInt(this.heightScene) / 2;
+      return { x, y };
+    }
+  },
   methods: {
-     drawMap,
+    drawMap,
+    testDraw() {
+      this.ctx.fillStyle = " #ff0000";
+      this.ctx.fil1Rect(10, 10, 50, 50);
+    },
+    moveOnMap() {
+      const target = event.target;
+      const id = target.id;
+      console.log(id)
+    }
   },
   mounted() {
-    this.ctx = this.$refs.scene.getContext('2d');
-    // this.ctx.fillStyle= 'red';
-    // this.ctx.fillRect(0,0, 100, 50);
+    this.ctx = this.$refs.scene.getContext("2d");
   }
 };
 </script>
