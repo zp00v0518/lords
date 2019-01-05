0afc9ef93f9701921dbd6a0e605dfc747fc918cc
Dmitriy <you@example.com>
2019-01-05 09:57:19 +0200

    base create Tooltip on GlobalMAp

--- src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
+++ src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
@@ -1,66 +1,119 @@
 <template>
   <div class="globalmap">
-    <canvas ref="scene" :width="widthScene" :height="heightScene"></canvas>
-    <button id="left" class="globalmap__move globalmap__move-left" type="button" @click="moveOnMap">left</button>
-    <button id="top" class="globalmap__move globalmap__move-top" type="button" @click="moveOnMap">top</button>
-    <button id="right" class="globalmap__move globalmap__move-right" type="button" @click="moveOnMap">right</button>
-    <button id="bottom" class="globalmap__move globalmap__move-bottom" type="button" @click="moveOnMap">bottom</button>
+    <Tooltip></Tooltip>
+    <canvas
+      ref="scene"
+      :width="widthScene"
+      :height="heightScene"
+      @mousemove="handlerMousemoveOnGlobalMap"
+    ></canvas>
+    <button
+      id="left"
+      class="globalmap__move globalmap__move-left"
+      type="button"
+      @click="moveOnMap"
+    >left</button>
+    <button
+      id="top"
+      class="globalmap__move globalmap__move-top"
+      type="button"
+      @click="moveOnMap"
+    >top</button>
+    <button
+      id="right"
+      class="globalmap__move globalmap__move-right"
+      type="button"
+      @click="moveOnMap"
+    >right</button>
+    <button
+      id="bottom"
+      class="globalmap__move globalmap__move-bottom"
+      type="button"
+      @click="moveOnMap"
+    >bottom</button>
   </div>
 </template>
 
 <script>
 import { drawMap } from "../modules";
+import Tooltip from "../../Tooltip";
+
 export default {
   name: "GlobalMap",
+  components: {
+    Tooltip
+  },
   props: ["widthScene", "heightScene"],
   data() {
     return {
       ctx: null,
-      currentMap: []
+      currentMap: [],
+      borderIsoMap: {
+        left: { x: 0, y: 0 },
+        top: { x: 0, y: 0 },
+        right: { x: 0, y: 0 },
+        bottom: { x: 0, y: 0 }
+      },
+      mouseCoords: { x: 0, y: 0 }
     };
   },
   created() {
-     this.currentMap = this.$store.state.globalMap.currentMap;
+    this.currentMap = this.$store.state.globalMap.currentMap;
   },
   watch: {
     "$store.state.globalMap.currentMap": function() {
       this.currentMap = this.$store.state.globalMap.currentMap;
       this.drawMap();
-    },
+      this.setBorderIsoMap();
+    }
   },
   computed: {
     tileWidth() {
       const widthParse = parseInt(this.widthScene) / 2;
-      const intermediate = widthParse  / (this.currentMap.length / 2);
-      return intermediate * 1.4
+      const intermediate = widthParse / (this.currentMap.length / 2);
+      return intermediate * 1.4;
       // return intermediate / (this.currentMap.length / 2) + intermediate;
     },
     isoCoords() {
-      const d = (this.tileWidth*this.currentMap.length)/2 //общая ширина всех ячеек /2
-      const x = (parseInt(this.widthScene)/2) - d; //от середины карты вычитываем половину длины всех ячеек
+      const d = (this.tileWidth * this.currentMap.length) / 2; //общая ширина всех ячеек /2
+      const x = parseInt(this.widthScene) / 2 - d; //от середины карты вычитываем половину длины всех ячеек
       const y = parseInt(this.heightScene) / 2;
       return { x, y };
     }
   },
   methods: {
     drawMap,
-    testDraw() {
-      this.ctx.fillStyle = " #ff0000";
-      this.ctx.fil1Rect(10, 10, 50, 50);
+    handlerMousemoveOnGlobalMap() {},
+    setBorderIsoMap() {
+      const currentLength = this.currentMap.length;
+      const height = this.tileWidth / 2;
+      this.borderIsoMap.left.x = this.isoCoords.x;
+      this.borderIsoMap.left.y = this.isoCoords.y;
+      this.borderIsoMap.top.x =
+        this.borderIsoMap.left.x + (this.tileWidth * currentLength) / 2;
+      this.borderIsoMap.top.y =
+        this.borderIsoMap.left.y - (height * currentLength) / 2;
+      this.borderIsoMap.right.x =
+        this.borderIsoMap.left.x + this.tileWidth * currentLength;
+      this.borderIsoMap.right.y = this.borderIsoMap.left.y;
+      this.borderIsoMap.bottom.x = this.borderIsoMap.top.x;
+      this.borderIsoMap.bottom.y =
+        this.borderIsoMap.left.y + (height * currentLength) / 2;
     },
     moveOnMap() {
       const target = event.target;
       const way = target.id;
       const message = {
-        type: 'moveGlobalMap',
-        way,
-      }
-      this.$ws.sendMessage(message)
+        type: "moveGlobalMap",
+        way
+      };
+      this.$ws.sendMessage(message);
     }
   },
   mounted() {
     this.ctx = this.$refs.scene.getContext("2d");
     this.drawMap();
+    this.setBorderIsoMap();
   }
 };
 </script>
---
+++ src/components/moleculs/Tooltip/Tooltip.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tooltip">
+    <div class="tooltip__player">Player:</div>
+    <div class="tooltip__town">Town:</div>
+    <div class="tooltip__coords">
+      <div class="tooltip__coords-x">X:</div>
+      <div class="tooltip__coords-y">Y:</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tooltip",
+  props: ["sectorGlobalMap"],
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style lang='scss' scoped>
+@import "tooltip.scss";
+</style>
