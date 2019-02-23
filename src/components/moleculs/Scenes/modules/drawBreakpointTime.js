b943e405da97d9d429942339c2cfe5b063e5c943
Dmitriy <you@example.com>
2019-02-23 10:01:13 +0200

    small change in draw timeline breakpoint

--- src/components/moleculs/Scenes/modules/drawBreakpointTime.js
+++ src/components/moleculs/Scenes/modules/drawBreakpointTime.js
@@ -1,14 +1,16 @@
 function drawBreakpointTime(ctx, breakpoint) {
   const height = ctx.canvas.height;
+  const y =  height - height / 2;
+  let step = 0;
   breakpoint.reduce((point, next, index) => {
-    console.log(index)
     if (index % 2 !== 0) {
       ctx.beginPath();
       ctx.moveTo(point, height);
-      ctx.lineTo(point, height - height / 2);
+      ctx.lineTo(point, y+step);
       ctx.stroke();
       ctx.closePath();
     }
+    step += 0.3
     return point + next;
   });
 }
