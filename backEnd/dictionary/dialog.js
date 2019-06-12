fd58edfe789756960149e1a72e464ef205095fe3
Dmitriy <you@example.com>
2019-06-12 07:57:39 +0300

    separate dictionary on module

---
+++ backEnd/dictionary/dialog.js
@@ -0,0 +1,32 @@
+const dialog = {
+  valueUpgrade: {
+    ru: 'Стоимость улучшения'
+  },
+  maxLvl: {
+    ru: 'Объект достиг максимального уровня'
+  },
+  notResources: {
+    ru: 'Не хватает ресурсов'
+  },
+  isUpgrade: {
+    ru: 'Действие невозможно - объект в данный момент улучшается'
+  },
+  upgradeDone: {
+    ru: 'Начато улучшение объекта'
+  },
+  questions: {
+    upgrade: {
+      ru: 'Вы хотите улучшить это здание?'
+    }
+  },
+  answer: {
+    yes: {
+      ru: 'да'
+    },
+    no: {
+      ru: 'нет'
+    }
+  }
+};
+
+module.exports = dialog;
