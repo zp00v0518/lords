3bf8e7dd4fcf36e78aa925be13d30eb4b27f0ce1
Dmitriy <you@example.com>
2019-07-14 16:09:18 +0300

    hjjul

--- backEnd/army/units/rapmart_units/index.js
+++ backEnd/army/units/rapmart_units/index.js
@@ -5,22 +5,23 @@ const createPegas = require('./pegas');
 const createDendroid = require('./dendroid');
 const createUnicorn = require('./unicorn');
 const createGreenDragon = require('./green_dragon');
+const listArmy = require('./list');
 
 const units = {
-  kentavr: createKentavr(1),
-  kentavr_2: createKentavr(2),
-  gnom: createGnom(1),
-  gnom_2: createGnom(2),
-  elf: createElf(1),
-  elf_2: createElf(2),
-  pegas: createPegas(1),
-  pegas_2: createPegas(2),
-  dendroid: createDendroid(1),
-  dendroid_2: createDendroid(2),
-  unicorn: createUnicorn(1),
-  unicorn_2: createUnicorn(2),
-  green_dragon: createGreenDragon(1),
-  green_dragon_2: createGreenDragon(2)
+  [listArmy.kentavr]: createKentavr(1),
+  [listArmy.kentavr_2]: createKentavr(2),
+  [listArmy.gnom]: createGnom(1),
+  [listArmy.gnom_2]: createGnom(2),
+  [listArmy.elf]: createElf(1),
+  [listArmy.elf_2]: createElf(2),
+  [listArmy.pegas]: createPegas(1),
+  [listArmy.pegas_2]: createPegas(2),
+  [listArmy.dendroid]: createDendroid(1),
+  [listArmy.dendroid_2]: createDendroid(2),
+  [listArmy.unicorn]: createUnicorn(1),
+  [listArmy.unicorn_2]: createUnicorn(2),
+  [listArmy.green_dragon]: createGreenDragon(1),
+  [listArmy.green_dragon_2]: createGreenDragon(2)
 };
 
 module.exports = units;
---
+++ backEnd/army/units/rapmart_units/list.js
@@ -0,0 +1,18 @@
+const list = {
+  kentavr: 'kentavr',
+  kentavr_2: 'kentavr_2',
+  gnom: 'gnom',
+  gnom_2: 'gnom_2',
+  elf: 'elf',
+  elf_2: 'elf_2',
+  pegas: 'pegas',
+  pegas_2: 'pegas_2',
+  dendroid: 'dendroid',
+  dendroid_2: 'dendroid_2',
+  unicorn: 'unicorn',
+  unicorn_2: 'unicorn_2',
+  green_dragon: 'green_dragon',
+  green_dragon_2: 'green_dragon_2',
+}
+
+module.exports = list;
