390e152926f355fafcae76238c84f9084d854826
Dmitriy <you@example.com>
2019-09-28 21:43:54 +0300

    add icon races

---
+++ backEnd/race/Rampart/images.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const images = {
+  bg: path.parse('frontEnd/img/for_canvas/towns/rampart/rampart.jpg'),
+  ico: path.parse('frontEnd/img/for_canvas/towns/rampart/ico/rampart.png')
+}
+
+console.log(images)
+
+module.exports = images;
