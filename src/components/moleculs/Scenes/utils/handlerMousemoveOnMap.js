51eb8dab66a197d03c435887942d306c99b657c3
Dmitriy <you@example.com>
2019-10-07 20:36:15 +0300

    fix old bug)

--- src/components/moleculs/Scenes/utils/handlerMousemoveOnMap.js
+++ src/components/moleculs/Scenes/utils/handlerMousemoveOnMap.js
@@ -2,9 +2,9 @@ function handlerMousemoveOnGlobalMap(event) {
   this.mouseCoords = this.getCursorPositionOnScene(event);
   if (this.checkMouseCoordsOnMap()) {
     const rombIndex = this.getTileCoordsOnMap();
-    if (this.currentTile !== this.currentMap[rombIndex.x][rombIndex.y]) {
+    const lineX = this.currentMap[rombIndex.x];
+    if (lineX && lineX[rombIndex.y] && this.currentTile !== lineX[rombIndex.y]) {
       this.drawMap();
-      this.drawHoverLine(rombIndex);
       this.currentTile = this.currentMap[rombIndex.x][rombIndex.y];
       this.showTooltip = true;
     }
