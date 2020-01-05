7bb7548268f38d8561e1d230820cd7dbef595e9e
Dmitriy <you@example.com>
2020-01-05 13:39:07 +0200

    recovery work app

--- config/index.js
+++ config/index.js
@@ -46,11 +46,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    // index: path.resolve(__dirname, '../frontEnd/dist/index.html'),
-    index: path.resolve(__dirname, '../frontEnd/game.html'), // подключить при билде
+    index: path.resolve(__dirname, '../frontEnd/dist/index.html'),
+    // index: path.resolve(__dirname, '../frontEnd/game.html'), // подключить при билде
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../frontEnd'),
+    assetsRoot: path.resolve(__dirname, '../frontEnd/dist'),
     // assetsRoot: path.resolve(__dirname, '../frontEnd'), // подключить при билде
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
