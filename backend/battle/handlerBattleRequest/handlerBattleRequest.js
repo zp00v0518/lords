093257c15f834c625e4831b4b33361f9dee333c6
Dmitry <dmitry.tarabanov@decimadigital.com>
2020-02-15 09:51:37 +0200

    add schema to handler

--- backend/battle/handlerBattleRequest/handlerBattleRequest.js
+++ backend/battle/handlerBattleRequest/handlerBattleRequest.js
@@ -1,7 +1,6 @@
 const { checkSchema } = require("../../template_modules");
 const { redirectMessage } = require("../../wsServer");
 
-
 function handlerBattleRequest(message, info) {
   const data = message.data;
   const { ws } = info.player;
@@ -18,6 +17,18 @@ module.exports = handlerBattleRequest;
 
 const schema = {
   attackHeroId: { type: "string", regExp: /^.{13,}\b/g },
-  target: { type: "string", regExp: /^.{10,}\b/g },
-  sectorIndex: { type: "number", min: 0 }
+  target: { type: "string", regExp: /^.{3,25}\b/g },
+  sectorIndex: { type: "number", min: 0 },
+  tileId: { type: "number", min: 0 },
+  army: {
+    type: "array",
+    all: {
+      type: "object",
+      fields: {
+        race: { type: "string", regExp: /^.{3,25}\b/g },
+        name: { type: "string", regExp: /^.{3,25}\b/g },
+        count: { type: "number", min: 1 }
+      }
+    }
+  }
 };
