6860d759b19baab97ea8af9d85de00d5ade60bdb
Dmitry <dmitry.tarabanov@decimadigital.com>
2020-04-12 12:58:12 +0300

    refactor into async/await

--- backend/town/handlerBuildNewTownRequest/handlerBuildNewTownRequest.js
+++ backend/town/handlerBuildNewTownRequest/handlerBuildNewTownRequest.js
@@ -5,7 +5,7 @@ const { Resources, deleteSource } = require('../../resources');
 const { Town, updateStateTown, getOneTownFromDB } = require('../../town');
 const setEventForBuildNewTown = require('./setEventForBuildNewTown');
 
-function handlerBuildNewTownRequest(message, info) {
+async function handlerBuildNewTownRequest(message, info) {
   const data = message.data;
   const { ws } = info.player;
   if (!checkSchema(data, schema)) {
@@ -25,58 +25,51 @@ function handlerBuildNewTownRequest(message, info) {
     type: message.type,
     data: {}
   };
-
-  getOneTownFromDB(serverName, data.targetSector)
-    .then(targetSector => {
-      if (!targetSector || targetSector.town) {
-        redirectMessage(ws);
-      }
-
-      getOneTownFromDB(serverName, curSector._id).then(sector => {
-        if (!sector) {
-          redirectMessage(ws);
-          return;
-        }
-        const sourseForBuild = Town.getSourceForNewTown(allSectors.length + 1);
-        let storage = sector.town.storage;
-        const haveSource = Resources.checkSource(sourseForBuild, storage.sources);
-        if (!haveSource) {
-          redirectMessage(ws);
-          return;
-        }
-
-        const { heroId } = data;
-        const heroes_in_town = sector.heroes;
-        if (!heroes_in_town || !heroes_in_town.some(i => i.toString() === heroId)) {
-          redirectMessage(ws);
-          return;
-        }
-
-        // необходимость получения героя из БД под вопросом
-        getHeroesFromDB(serverName, { heroId }).then(hero => {
-          if (!hero.active) {
-            redirectMessage(ws);
-            return;
-          }
-          setEventForBuildNewTown(sector, targetSector, hero).then(event => {
-
-            storage = deleteSource(sourseForBuild, storage);
-            updateStateTown(sector).then(() => {
-              response.data.targetSector = targetSector;
-              response.data.hero = hero;
-              response.data.sector = sector;
-              response.data.sourseForBuild = sourseForBuild;
-              response.data.event = event;
-              sendWSMessage(ws, response);
-            });
-          });
-        });
-      });
-    })
-    .catch(err => {
-      console.log(err);
+  try {
+    const targetSector = await getOneTownFromDB(serverName, data.targetSector);
+    if (!targetSector || targetSector.town) {
       redirectMessage(ws);
-    });
+    }
+
+    const sector = await getOneTownFromDB(serverName, curSector._id);
+    if (!sector) {
+      redirectMessage(ws);
+      return;
+    }
+    const sourseForBuild = Town.getSourceForNewTown(allSectors.length + 1);
+    let storage = sector.town.storage;
+    const haveSource = Resources.checkSource(sourseForBuild, storage.sources);
+    if (!haveSource) {
+      redirectMessage(ws);
+      return;
+    }
+
+    const { heroId } = data;
+    const heroes_in_town = sector.heroes;
+    if (!heroes_in_town || !heroes_in_town.some(i => i.toString() === heroId)) {
+      redirectMessage(ws);
+      return;
+    }
+
+    // необходимость получения героя из БД под вопросом
+    const hero = await getHeroesFromDB(serverName, { heroId });
+    if (!hero.active) {
+      redirectMessage(ws);
+      return;
+    }
+    const event = await setEventForBuildNewTown(sector, targetSector, hero);
+    storage = deleteSource(sourseForBuild, storage);
+    const updateTownResult = await updateStateTown(sector);
+    response.data.targetSector = targetSector;
+    response.data.hero = hero;
+    response.data.sector = sector;
+    response.data.sourseForBuild = sourseForBuild;
+    response.data.event = event;
+    sendWSMessage(ws, response);
+  } catch (err) {
+    console.log(err);
+    redirectMessage(ws);
+  }
 }
 
 const schema = {
