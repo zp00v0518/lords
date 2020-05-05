70a56a4d22f3634677480faf114648eec0780fc7
Dmitry <dmitry.tarabanov@decimadigital.com>
2020-05-05 19:36:11 +0300

    fix path

--- backend/army/handlerBuyUnits/handlerBuyUnits.js
+++ backend/army/handlerBuyUnits/handlerBuyUnits.js
@@ -2,7 +2,7 @@
 const { checkSchema } = require('../../template_modules');
 const { redirectMessage, sendWSMessage } = require('../../wsServer');
 const { Race } = require('../../race');
-const Army = require('../army/Army.js');
+const Army = require('../Army/Army.js');
 const { checkSource } = require('../../resources');
 const checkUnitInBarraks = require('./../checkUnitInBarraks');
 const { deleteSource } = require('../../resources');
