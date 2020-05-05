41eb0ccbaae5769778c4d4826cc1bfc3efc03ba2
Dmitry <dmitry.tarabanov@decimadigital.com>
2020-05-05 20:50:08 +0300

    fix path

--- get_files.js
+++ get_files.js
@@ -1,30 +1,31 @@
 /* формируется список путей всех картинок, который расположены в папке frontend/img, 
 потом этот список загружается через sourceLoader, для использования на canvas
 */
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 const startPath = 'img/for_canvas';
 let arrPath = [];
 
-function getPath(nextPath){
+function getPath(nextPath) {
   const f = fs.readdirSync(nextPath);
   f.forEach(item => {
-    const checkPath = path.join(nextPath, item)
+    const checkPath = path.join(nextPath, item);
     const d = fs.statSync(checkPath);
-    if (!d.isDirectory()){
-      arrPath.push(checkPath)
+    if (!d.isDirectory()) {
+      arrPath.push(checkPath);
     } else {
-      getPath(checkPath)
+      getPath(checkPath);
     }
-  })
+  });
 }
-const normStr = path.normalize('frontEnd\\');
-console.log(normStr);
+const normStr = 'frontEnd';
 
-getPath('./frontEnd/'+startPath);
+getPath('./frontEnd/' + startPath);
 arrPath = arrPath.map(item => {
-  const f = path.normalize(item);
-  console.log(f.replace(normStr, ""));
-  return f.replace(normStr, "")
-})
-fs.writeFileSync("./frontEnd/js/list_img_for_canvas.js", `const list_img_for_canvas = ${JSON.stringify(arrPath)}`, 'utf-8')
+  return item.replace(normStr + path.sep, '');
+});
+fs.writeFileSync(
+  './frontEnd/js/list_img_for_canvas.js',
+  `const list_img_for_canvas = ${JSON.stringify(arrPath)}`,
+  'utf-8'
+);
