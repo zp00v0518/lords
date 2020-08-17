ca401962a1c1e9b25ddc5e8e741bf2ecfc14f482
Dmitry <dmitry.tarabanov@decimadigital.com>
2020-08-17 21:41:55 +0300

    create user

--- backend/prepareToStart/demo.js
+++ backend/prepareToStart/demo.js
@@ -1,5 +1,30 @@
+const insert = require('../workWithMongoDB/insertDB.js');
+const insertDB = new insert();
+const config = require('../config');
 const userCreate = require('../user/userCreate');
 const u_1 = require('./user_1');
 
 const user_1 = userCreate(u_1);
-console.log(user_1);
+
+function insertDemoUserToDB(user) {
+  const options = {
+    collectionName: config.db.collections.users,
+    doc: user
+  };
+  insertDB.one(options).then(result => {
+    console.log('insert Demo User');
+    insertDB.close();
+  });
+}
+
+function startCreate() {
+  if (!config.db.check) {
+    setTimeout(() => {
+      startCreate();
+    }, 100);
+  } else {
+    insertDemoUserToDB(user_1);
+  }
+}
+
+startCreate();
