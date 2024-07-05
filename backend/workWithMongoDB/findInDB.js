1c13af3b1f0e156db419bc1b3b17fa7e2fe32e48
Dmitriy <johndoe@example.com>
2024-07-05 10:49:57 +0300

    refactoring

--- backend/workWithMongoDB/findInDB.js
+++ backend/workWithMongoDB/findInDB.js
@@ -5,7 +5,7 @@ mongo.connect({ dbName: config.db.name });
 
 function findInDB() {
   this.one = async function (options) {
-    let collection = mongo.open(options.collectionName);
+    let collection = await mongo.open(options.collectionName);
     let needFields = options.needFields || null;
     let query = options.query || null;
     let skip = options.skip || null;
@@ -13,41 +13,64 @@ function findInDB() {
     const findResult = await collection.findOne(query, { projection: needFields, skip: skip });
     return findResult;
   };
-  this.all = function (options, callback) {
-    return new Promise((resolve, reject) => {
-      let collection = mongo.open(options.collectionName);
-      let sort = options.sort || undefined;
-      let limit = options.limit || 0;
-      let skip = options.skip || 0;
-      let needFields = options.needFields || null;
-      let query = options.query || null;
-      let comment = options.comment || null;
-      collection
-        .find(query, {
-          projection: needFields,
-          sort: sort,
-          skip: skip,
-          limit: limit,
-          comment: comment
-        })
-        .toArray((err, itog) => {
-          if (err) {
-            reject(err);
-            throw err;
-          }
-          let findResult = {
-            result: itog,
-            sort: sort,
-            limit: limit,
-            skip: skip
-          };
-          resolve(findResult);
-          return callback ? callback(findResult) : findResult;
-        });
-    });
+  this.all = async function (options, callback) {
+    let collection = await mongo.open(options.collectionName);
+    let sort = options.sort || undefined;
+    let limit = options.limit || 0;
+    let skip = options.skip || 0;
+    let needFields = options.needFields || null;
+    let query = options.query || null;
+    let comment = options.comment || null;
+    let itog = await collection.find(query, {
+      projection: needFields,
+      sort: sort,
+      skip: skip,
+      limit: limit,
+      comment: comment
+    })
+
+    itog = await itog.toArray()
+    let findResult = {
+      result: itog,
+      sort: sort,
+      limit: limit,
+      skip: skip
+    };
+    return callback ? callback(findResult) : findResult;
+    // return new Promise((resolve, reject) => {
+    //   let collection = mongo.open(options.collectionName);
+    //   let sort = options.sort || undefined;
+    //   let limit = options.limit || 0;
+    //   let skip = options.skip || 0;
+    //   let needFields = options.needFields || null;
+    //   let query = options.query || null;
+    //   let comment = options.comment || null;
+    //   collection
+    //     .find(query, {
+    //       projection: needFields,
+    //       sort: sort,
+    //       skip: skip,
+    //       limit: limit,
+    //       comment: comment
+    //     })
+    //     .toArray((err, itog) => {
+    //       if (err) {
+    //         reject(err);
+    //         throw err;
+    //       }
+    //       let findResult = {
+    //         result: itog,
+    //         sort: sort,
+    //         limit: limit,
+    //         skip: skip
+    //       };
+    //       resolve(findResult);
+    //       return callback ? callback(findResult) : findResult;
+    //     });
+    // });
   };
   this.count = async function (options) {
-    let collection = mongo.open(options.collectionName);
+    let collection = await mongo.open(options.collectionName);
     let query = options.query || {};
     const count = await collection.countDocuments(query);
     return count;
