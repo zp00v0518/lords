e7aa381d785611490a6a88a07e327c1a74e836c9
Dmitriy <johndoe@example.com>
2024-07-07 19:22:59 +0300

    change scene size

--- src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
+++ src/components/moleculs/Scenes/GlobalMap/GlobalMap.vue
@@ -66,14 +66,14 @@
 </template>
 
 <script>
-import Tooltip from '../../Tooltip'
-import PopupTown from './modules/PopupTown.vue'
-import GoTo from './modules/GoTo.vue'
-import { currentSector } from '../../../mixins'
-import drawHeroMixin from '../mixins/drawHeroMixin.vue'
-import baseMixins from '../mixins/baseMixins.vue'
-import { algebra } from '../../../../utils'
-import { iso } from '../utils'
+import Tooltip from '../../Tooltip';
+import PopupTown from './modules/PopupTown.vue';
+import GoTo from './modules/GoTo.vue';
+import { currentSector } from '../../../mixins';
+import drawHeroMixin from '../mixins/drawHeroMixin.vue';
+import baseMixins from '../mixins/baseMixins.vue';
+import { algebra } from '../../../../utils';
+import { iso } from '../utils';
 
 export default {
   name: 'GlobalMap',
@@ -86,199 +86,200 @@ export default {
       popupTown: {
         show: false,
         targetSector: null,
-        isSelf: true
+        isSelf: true,
       },
       moveBtnCoords: {
         left: { top: 0, left: 0 },
         right: { top: 0, left: 0 },
         top: { top: 0, left: 0 },
-        bottom: { top: 0, left: 0 }
-      }
-    }
+        bottom: { top: 0, left: 0 },
+      },
+    };
   },
   created() {
-    this.currentMap = this.$store.state.globalMap.currentMap
-    this.$bus.$on('rerender_global_map', this.drawMap)
+    this.currentMap = this.$store.state.globalMap.currentMap;
+    this.$bus.$on('rerender_global_map', this.drawMap);
   },
   beforeUnmount() {
-    this.$bus.$off('rerender_global_map', this.drawMap)
+    this.$bus.$off('rerender_global_map', this.drawMap);
   },
   watch: {
     '$store.state.globalMap.currentMap': function () {
-      const { deepClone, $store } = this
-      this.currentMap = deepClone($store.state.globalMap.currentMap)
-      this.isDisabledMove = false
-      this.drawMap()
+      const { deepClone, $store } = this;
+      this.currentMap = deepClone($store.state.globalMap.currentMap);
+      this.isDisabledMove = false;
+      this.drawMap();
       // this.setBorderIsoMap();
     },
     eventList: {
       deep: true,
       handler() {
-        this.drawMap()
-      }
-    }
+        this.drawMap();
+      },
+    },
   },
   computed: {
     zoomText() {
-      return this.zoom === 1 ? 'Zoom +' : 'Zoom - '
+      return this.zoom === 1 ? 'Zoom +' : 'Zoom - ';
     },
     zoom() {
-      return this.$store.state.globalMap.zoom
+      return this.$store.state.globalMap.zoom;
     },
     tileWidth() {
-      const widthParse = parseInt(this.widthScene) / 2
-      const intermediate = widthParse / (this.currentMap.length / 2)
-      return intermediate * 0.9
+      const widthParse = parseInt(this.widthScene) / 2;
+      const intermediate = widthParse / (this.currentMap.length / 2);
+      return intermediate * 0.95;
+      // return intermediate * 0.9;
       // return intermediate / (this.currentMap.length / 2) + intermediate;
     },
     isoCoords() {
-      const x = parseInt(this.widthScene) / 2
-      const y = (parseInt(this.heightScene) / 100) * 10
-      return { x, y }
+      const x = parseInt(this.widthScene) / 2;
+      const y = (parseInt(this.heightScene) / 100) * 10;
+      return { x, y };
     },
     eventList() {
-      const list = this.$store.state.timeline.eventsList
-      const { Event } = this.$store.state.globalConfig.all
-      const { deepClone } = this
+      const list = this.$store.state.timeline.eventsList;
+      const { Event } = this.$store.state.globalConfig.all;
+      const { deepClone } = this;
       const d = list.filter((item) => {
-        return item.mode === Event.mode.global || item.mode === Event.mode.hidden
-      })
-      return deepClone(d)
+        return item.mode === Event.mode.global || item.mode === Event.mode.hidden;
+      });
+      return deepClone(d);
     },
     curUser() {
-      return this.$store.state.user.user
-    }
+      return this.$store.state.user.user;
+    },
   },
   methods: {
     drawAnotherObjects() {
-      this.drawMoveHero()
+      this.drawMoveHero();
     },
     changeZoom(event) {
-      this.$store.commit('CHANGE__ZOOM')
-      this.moveOnMap(event)
+      this.$store.commit('CHANGE__ZOOM');
+      this.moveOnMap(event);
     },
     moveOnMap(event) {
-      if (this.isDisabledMove) return
-      this.isDisabledMove = true
-      const target = event.target
-      const way = target.id
+      if (this.isDisabledMove) return;
+      this.isDisabledMove = true;
+      const target = event.target;
+      const way = target.id;
       const message = {
         type: 'moveGlobalMap',
         way,
-        zoom: this.zoom
-      }
-      this.$ws.sendMessage(message)
+        zoom: this.zoom,
+      };
+      this.$ws.sendMessage(message);
     },
     handlerClickOnGlobalMap($event) {
-      const tileTypes = this.globalConfig.all.WorldMap.types
-      const { currentTile, $store, deepClone, currentSector } = this
-      const { user } = $store.state
+      const tileTypes = this.globalConfig.all.WorldMap.types;
+      const { currentTile, $store, deepClone, currentSector } = this;
+      const { user } = $store.state;
       if (currentTile.type === tileTypes.empty.id) {
         const payload = {
           data: {
             targetTile: deepClone(currentTile),
-            initSector: deepClone(currentSector)
+            initSector: deepClone(currentSector),
           },
-          type: 'worldMapRegion'
-        }
-        $store.commit('DIALOG_SHOW', payload)
+          type: 'worldMapRegion',
+        };
+        $store.commit('DIALOG_SHOW', payload);
       } else if (currentTile.type === tileTypes.town.id && currentTile.userId === user.id) {
-        const { sectors } = $store.state.userSectors
-        const to_be = sectors.find((i) => i._id === currentTile._id)
+        const { sectors } = $store.state.userSectors;
+        const to_be = sectors.find((i) => i._id === currentTile._id);
         if (to_be && to_be._id !== currentSector._id) {
-          this.popupTown.show = true
-          this.popupTown.isSelf = true
-          this.popupTown.targetSector = deepClone(currentTile)
+          this.popupTown.show = true;
+          this.popupTown.isSelf = true;
+          this.popupTown.targetSector = deepClone(currentTile);
         }
       } else if (currentTile.type === tileTypes.town.id && currentTile.userId !== user.id) {
-        this.popupTown.show = true
-        this.popupTown.isSelf = false
-        this.popupTown.targetSector = deepClone(currentTile)
+        this.popupTown.show = true;
+        this.popupTown.isSelf = false;
+        this.popupTown.targetSector = deepClone(currentTile);
       }
     },
     drawMoveHero() {
-      if (this.mode && this.mode !== 'global') return
-      const { ctx, eventList, currentMap, tileWidth, settings, getTileByCoords, curUser } = this
-      ctx.fillStyle = settings.baseColor
+      if (this.mode && this.mode !== 'global') return;
+      const { ctx, eventList, currentMap, tileWidth, settings, getTileByCoords, curUser } = this;
+      ctx.fillStyle = settings.baseColor;
       eventList.forEach((event) => {
-        const { target, init } = event
-        const startCoords = { x: init.x, y: init.y }
-        const endCoords = { x: target.x, y: target.y }
-        const WorldMap = this.globalConfig.all.WorldMap
-        const sizeMap = WorldMap.numSectionGlobalMap
-        const width = this.tileWidth
-        const height = width / 2
-        let startTile = getTileByCoords(currentMap, startCoords.x, startCoords.y)
-        let endTile = getTileByCoords(currentMap, endCoords.x, endCoords.y)
+        const { target, init } = event;
+        const startCoords = { x: init.x, y: init.y };
+        const endCoords = { x: target.x, y: target.y };
+        const WorldMap = this.globalConfig.all.WorldMap;
+        const sizeMap = WorldMap.numSectionGlobalMap;
+        const width = this.tileWidth;
+        const height = width / 2;
+        let startTile = getTileByCoords(currentMap, startCoords.x, startCoords.y);
+        let endTile = getTileByCoords(currentMap, endCoords.x, endCoords.y);
         if (startTile && !endTile) {
-          endTile = algebra.getMinPath(startTile, endCoords, sizeMap)
-          iso.addCenterPoints(startTile, endTile, height)
+          endTile = algebra.getMinPath(startTile, endCoords, sizeMap);
+          iso.addCenterPoints(startTile, endTile, height);
         } else if (!startTile && endTile) {
-          startTile = algebra.getMinPath(endTile, startCoords, sizeMap)
-          iso.addCenterPoints(endTile, startTile, height)
+          startTile = algebra.getMinPath(endTile, startCoords, sizeMap);
+          iso.addCenterPoints(endTile, startTile, height);
         } else if (!startTile && !endTile) {
-          startTile = algebra.getMinPath(currentMap[0][0], startCoords, sizeMap)
-          endTile = algebra.getMinPath(startTile, endCoords, sizeMap)
-          iso.addCenterPoints(currentMap[0][0], startTile, height)
-          iso.addCenterPoints(startTile, endTile, height)
+          startTile = algebra.getMinPath(currentMap[0][0], startCoords, sizeMap);
+          endTile = algebra.getMinPath(startTile, endCoords, sizeMap);
+          iso.addCenterPoints(currentMap[0][0], startTile, height);
+          iso.addCenterPoints(startTile, endTile, height);
         }
-        const baseCoords = [startTile.centerX, startTile.centerY, endTile.centerX, endTile.centerY]
-        const fullLength = algebra.getStraightLength(...baseCoords)
-        let heroLength = this.getLengthHeroOnStraight(fullLength, event.start, event.end)
-        if (heroLength > fullLength) heroLength = fullLength
-        const step = tileWidth / 4
-        const color = init.color
+        const baseCoords = [startTile.centerX, startTile.centerY, endTile.centerX, endTile.centerY];
+        const fullLength = algebra.getStraightLength(...baseCoords);
+        let heroLength = this.getLengthHeroOnStraight(fullLength, event.start, event.end);
+        if (heroLength > fullLength) heroLength = fullLength;
+        const step = tileWidth / 4;
+        const color = init.color;
         // console.log(`%c ${color}`, `background: ${color}`);
-        ctx.fillStyle = color || curUser.color
+        ctx.fillStyle = color || curUser.color;
         for (let i = 0; i < fullLength + 1; i += step) {
-          const coords = algebra.getPointOnStraight(...baseCoords, i)
-          const { viewportPath } = this
-          const isPoint = ctx.isPointInPath(viewportPath, coords.x, coords.y)
-          if (!isPoint) continue
-          ctx.beginPath()
-          const r = i > heroLength ? 2 : 4
-          ctx.arc(coords.x, coords.y, r, 0, 2 * Math.PI)
-          ctx.fill()
-          ctx.closePath()
+          const coords = algebra.getPointOnStraight(...baseCoords, i);
+          const { viewportPath } = this;
+          const isPoint = ctx.isPointInPath(viewportPath, coords.x, coords.y);
+          if (!isPoint) continue;
+          ctx.beginPath();
+          const r = i > heroLength ? 2 : 4;
+          ctx.arc(coords.x, coords.y, r, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.closePath();
         }
-        const heroCoords = algebra.getPointOnStraight(...baseCoords, heroLength)
-        this.drawHeroOnMap(ctx, heroCoords)
-      })
+        const heroCoords = algebra.getPointOnStraight(...baseCoords, heroLength);
+        this.drawHeroOnMap(ctx, heroCoords);
+      });
     },
     getMoveBtnPositions() {
-      const { borderIsoMap, moveBtnCoords } = this
-      const { top, left, right, bottom } = borderIsoMap
-      let coords = addPXToCoords(algebra.getPointOnStraight(top.x, top.y, left.x, left.y, '50%'))
-      moveBtnCoords.left.left = coords.x
-      moveBtnCoords.left.top = coords.y
-      coords = addPXToCoords(algebra.getPointOnStraight(top.x, top.y, right.x, right.y, '50%'))
-      moveBtnCoords.top.left = coords.x
-      moveBtnCoords.top.top = coords.y
+      const { borderIsoMap, moveBtnCoords } = this;
+      const { top, left, right, bottom } = borderIsoMap;
+      let coords = addPXToCoords(algebra.getPointOnStraight(top.x, top.y, left.x, left.y, '50%'));
+      moveBtnCoords.left.left = coords.x;
+      moveBtnCoords.left.top = coords.y;
+      coords = addPXToCoords(algebra.getPointOnStraight(top.x, top.y, right.x, right.y, '50%'));
+      moveBtnCoords.top.left = coords.x;
+      moveBtnCoords.top.top = coords.y;
       coords = addPXToCoords(
-        algebra.getPointOnStraight(right.x, right.y, bottom.x, bottom.y, '50%')
-      )
-      moveBtnCoords.right.left = coords.x
-      moveBtnCoords.right.top = coords.y
-      coords = addPXToCoords(algebra.getPointOnStraight(bottom.x, bottom.y, left.x, left.y, '50%'))
-      moveBtnCoords.bottom.left = coords.x
-      moveBtnCoords.bottom.top = coords.y
+        algebra.getPointOnStraight(right.x, right.y, bottom.x, bottom.y, '50%'),
+      );
+      moveBtnCoords.right.left = coords.x;
+      moveBtnCoords.right.top = coords.y;
+      coords = addPXToCoords(algebra.getPointOnStraight(bottom.x, bottom.y, left.x, left.y, '50%'));
+      moveBtnCoords.bottom.left = coords.x;
+      moveBtnCoords.bottom.top = coords.y;
 
       function addPXToCoords(obj) {
         Object.keys(obj).forEach((key) => {
-          const value = obj[key]
-          obj[key] = value + 'px'
-        })
-        return obj
+          const value = obj[key];
+          obj[key] = value + 'px';
+        });
+        return obj;
       }
-    }
+    },
   },
   mounted() {
-    this.ctx = this.$refs.scene.getContext('2d')
-    this.drawMap()
-    this.setBorderIsoMap()
-    this.getMoveBtnPositions()
-  }
-}
+    this.ctx = this.$refs.scene.getContext('2d');
+    this.drawMap();
+    this.setBorderIsoMap();
+    this.getMoveBtnPositions();
+  },
+};
 </script>
 
 <style lang="scss">
--- src/components/moleculs/Scenes/RegionMap/RegionMap.vue
+++ src/components/moleculs/Scenes/RegionMap/RegionMap.vue
@@ -21,17 +21,17 @@
 </template>
 
 <script>
-import TooltipRegion from '../../TooltipRegion'
-import { currentSector } from '../../../mixins'
-import drawHeroMixin from '../mixins/drawHeroMixin.vue'
-import baseMixins from '../mixins/baseMixins.vue'
-import { algebra } from '../../../../utils'
+import TooltipRegion from '../../TooltipRegion';
+import { currentSector } from '../../../mixins';
+import drawHeroMixin from '../mixins/drawHeroMixin.vue';
+import baseMixins from '../mixins/baseMixins.vue';
+import { algebra } from '../../../../utils';
 
 export default {
   name: 'RegionMap',
   mixins: [currentSector, drawHeroMixin, baseMixins],
   components: {
-    TooltipRegion
+    TooltipRegion,
   },
   props: {
     widthScene: { default: 0 },
@@ -39,27 +39,27 @@ export default {
     regionMap: null,
     mode: { type: String, default: 'global' },
     sectorInfo: { default: null },
-    customHoverFunc: { type: Function, default: null }
+    customHoverFunc: { type: Function, default: null },
   },
   data() {
     return {
       sceneWidth: this.widthScene,
-      sceneHeight: this.heightScene
-    }
+      sceneHeight: this.heightScene,
+    };
   },
   created() {
     if (!this.regionMap) {
-      this.currentMap = this.currentSector.region
+      this.currentMap = this.currentSector.region;
     } else {
-      this.currentMap = this.regionMap
+      this.currentMap = this.regionMap;
     }
   },
   watch: {
     'currentSector.region': function (e) {
       if (this.mode === 'global') {
-        const { deepClone } = this
-        this.currentMap = deepClone(e)
-        this.drawMap()
+        const { deepClone } = this;
+        this.currentMap = deepClone(e);
+        this.drawMap();
         // this.setBorderIsoMap();
       }
     },
@@ -67,133 +67,135 @@ export default {
       deep: true,
       handler() {
         if (this.mode === 'global') {
-          this.drawMap()
+          this.drawMap();
         }
-      }
-    }
+      },
+    },
   },
   computed: {
     eventList() {
-      const list = this.$store.state.timeline.eventsList
-      const { Battle, Event } = this.$store.state.globalConfig.all
-      const { deepClone } = this
-      const b_types = Battle.types
-      const e_types = Event.types
+      const list = this.$store.state.timeline.eventsList;
+      const { Battle, Event } = this.$store.state.globalConfig.all;
+      const { deepClone } = this;
+      const b_types = Battle.types;
+      const e_types = Event.types;
       const d = list.filter((item) => {
         if (item.data && item.data.typeBattle === b_types.region.name) {
-          return item.type === e_types.battle || item.type === e_types.backToTown
+          return item.type === e_types.battle || item.type === e_types.backToTown;
         }
-      })
-      return deepClone(d)
+      });
+      return deepClone(d);
     },
     tileWidth() {
-      const { ctx, mode, currentMap } = this
-      if (!ctx) return 0
-      const widthParse = parseInt(this.sceneWidth) / 2
-      const intermediate = currentMap.length === 0 ? 0 : widthParse / (currentMap.length / 2)
+      const { ctx, mode, currentMap } = this;
+      if (!ctx) return 0;
+      const widthParse = parseInt(this.sceneWidth) / 2;
+      const intermediate = currentMap.length === 0 ? 0 : widthParse / (currentMap.length / 2);
       if (mode !== 'global') {
-        return intermediate * 0.6
+        return intermediate * 0.63;
+        // return intermediate * 0.6;
       }
-      return intermediate * 0.9
+      return intermediate * 0.95;
+      // return intermediate * 0.9;
     },
     isoCoords() {
-      const x = parseInt(this.sceneWidth) / 2
-      const y = (parseInt(this.sceneHeight) / 100) * 10
-      return { x, y }
-    }
+      const x = parseInt(this.sceneWidth) / 2;
+      const y = (parseInt(this.sceneHeight) / 100) * 10;
+      return { x, y };
+    },
   },
   methods: {
     handlerMouseLeave() {
-      this.hideTooltip()
+      this.hideTooltip();
     },
     handlerClick() {
-      if (!this.cursorOnScene) return
-      const { currentTile, globalConfig } = this
+      if (!this.cursorOnScene) return;
+      const { currentTile, globalConfig } = this;
       if (this.mode === 'dialog') {
-        this.$emit('click', { target: currentTile })
-        return
+        this.$emit('click', { target: currentTile });
+        return;
       }
-      if (currentTile.type === 1 || currentTile.type === 3) return
+      if (currentTile.type === 1 || currentTile.type === 3) return;
       if (this.currentTile.type === 0) {
-        if (currentTile.army && currentTile.army.length === 0) return
+        if (currentTile.army && currentTile.army.length === 0) return;
         const payload = {
           title: 'Настройка битвы',
           data: {
             defenseArmy: currentTile.army,
             target: 'region',
-            tile: currentTile
+            tile: currentTile,
           },
-          type: 'dialogBattle'
-        }
-        this.$store.commit('DIALOG_SHOW', payload)
-        return
+          type: 'dialogBattle',
+        };
+        this.$store.commit('DIALOG_SHOW', payload);
+        return;
       }
-      const nameRegion = globalConfig.all.Region.typeList[currentTile.type]
-      const building = currentTile.sector
-      const typeBuilding = building.type
+      const nameRegion = globalConfig.all.Region.typeList[currentTile.type];
+      const building = currentTile.sector;
+      const typeBuilding = building.type;
       const payload = {
         title: this.gloss[nameRegion].type[typeBuilding].name.txt,
         data: {
           building,
           x: currentTile.x,
-          y: currentTile.y
+          y: currentTile.y,
         },
-        type: 'upgradeRegion'
-      }
-      this.$store.commit('DIALOG_SHOW', payload)
+        type: 'upgradeRegion',
+      };
+      this.$store.commit('DIALOG_SHOW', payload);
     },
     drawAnotherObjects() {
-      this.drawMoveHero()
+      this.drawMoveHero();
     },
     setSizeScene() {
-      const { body } = this.$refs
-      const styles = body.getBoundingClientRect()
+      const { body } = this.$refs;
+      const styles = body.getBoundingClientRect();
       if (!this.widthScene) {
-        this.sceneWidth = styles.width + 'px'
+        this.sceneWidth = styles.width + 'px';
       }
       if (!this.heightScene) {
-        this.sceneHeight = styles.height + 'px'
+        this.sceneHeight = styles.height + 'px';
       }
     },
     drawMoveHero() {
-      if (this.mode && this.mode !== 'global') return
-      const { ctx, eventList, currentMap, tileWidth, settings, getTileByCoords } = this
-      ctx.fillStyle = settings.baseColor
+      if (this.mode && this.mode !== 'global') return;
+      const { ctx, eventList, currentMap, tileWidth, settings, getTileByCoords } = this;
+      ctx.fillStyle = settings.baseColor;
       eventList.forEach((event) => {
-        const { data, target } = event
-        if (target.sector !== this.currentSector._id) return
-        const { startCoords, endCoords } = data
-        const startTile = getTileByCoords(currentMap, startCoords.x, startCoords.y)
-        const endTile = getTileByCoords(currentMap, endCoords.x, endCoords.y)
-        const baseCoords = [startTile.centerX, startTile.centerY, endTile.centerX, endTile.centerY]
-        const fullLength = algebra.getStraightLength(...baseCoords)
-        let heroLength = this.getLengthHeroOnStraight(fullLength, event.start, event.end)
-        if (heroLength > fullLength) heroLength = fullLength
-        const step = tileWidth / 4
-        const color = this.$store.getters.getUserColor
-        ctx.fillStyle = color
+        const { data, target } = event;
+        if (target.sector !== this.currentSector._id) return;
+        const { startCoords, endCoords } = data;
+        const startTile = getTileByCoords(currentMap, startCoords.x, startCoords.y);
+        const endTile = getTileByCoords(currentMap, endCoords.x, endCoords.y);
+        const baseCoords = [startTile.centerX, startTile.centerY, endTile.centerX, endTile.centerY];
+        const fullLength = algebra.getStraightLength(...baseCoords);
+        let heroLength = this.getLengthHeroOnStraight(fullLength, event.start, event.end);
+        if (heroLength > fullLength) heroLength = fullLength;
+        const step = tileWidth / 4;
+        const color = this.$store.getters.getUserColor;
+        ctx.fillStyle = color;
         for (let i = 0; i < fullLength + 1; i += step) {
-          const coords = algebra.getPointOnStraight(...baseCoords, i)
-          ctx.beginPath()
-          const r = i > heroLength ? 2 : 4
-          ctx.arc(coords.x, coords.y, r, 0, 2 * Math.PI)
-          ctx.fill()
-          ctx.closePath()
+          const coords = algebra.getPointOnStraight(...baseCoords, i);
+          ctx.beginPath();
+          const r = i > heroLength ? 2 : 4;
+          ctx.arc(coords.x, coords.y, r, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.closePath();
         }
-        const heroCoords = algebra.getPointOnStraight(...baseCoords, heroLength)
-        this.drawHeroOnMap(ctx, heroCoords)
-      })
-    }
+        const heroCoords = algebra.getPointOnStraight(...baseCoords, heroLength);
+        this.drawHeroOnMap(ctx, heroCoords);
+      });
+    },
   },
   mounted() {
-    this.ctx = this.$refs.scene.getContext('2d')
+    this.ctx = this.$refs.scene.getContext('2d');
     this.$nextTick(() => {
-      this.setSizeScene()
-      this.setBorderIsoMap()
-      requestAnimationFrame(this.drawMap)
-    })
-  }
-}
+      this.setSizeScene();
+      this.setBorderIsoMap();
+      requestAnimationFrame(this.drawMap);
+    });
+  },
+};
 </script>
 
 <style lang="scss">
