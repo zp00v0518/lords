5031ef31e21aa06e6abd6cbcd1819e540e3466c2
Dmitriy <johndoe@example.com>
2024-07-08 07:16:12 +0300

    fixed calcValueUnitInBarraks

--- backend/army/calcValueUnitInBarraks.js
+++ backend/army/calcValueUnitInBarraks.js
@@ -2,6 +2,7 @@ function calcValueUnitInBarraks(barraks, timeEnd = new Date().getTime()) {
   const addUnits = gameVariables.timer.addUnits;
   const workInfo = barraks.work;
   if (workInfo.lastCalc !== 0) {
+    if (Number.isNaN(workInfo.nowValue)) workInfo.nowValue = 0;
     const ost = timeEnd - workInfo.lastCalc;
     const d = (workInfo.addValue / addUnits) * ost;
     workInfo.nowValue += d;
